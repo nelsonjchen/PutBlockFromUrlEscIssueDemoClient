3e3c4dbd2f2b829ac933d7e8713053e2cd13d6dd
Nelson Chen <crazysim@gmail.com>
2021-11-20 20:35:27 -0800

    Initial restore from mind

--- PutBlockFromUrlEscIssueDemoClient/Program.cs
+++ PutBlockFromUrlEscIssueDemoClient/Program.cs
@@ -1,5 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Threading;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
 
 Console.WriteLine("Hello, World!");
+
+string? connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+if (connectionString == null) 
+{
+    Console.WriteLine("Set the environment variable AZURE_STORAGE_CONNECTION_STRING to your storage account connection string.");
+    return;
+}
+
+BlobServiceClient blobServiceClient = new(connectionString);
+BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("test-storage");
+BlockBlobClient blobClient = containerClient.GetBlockBlobClient("test-file");
+Uri uri = new("http://example.com");
+var response = blobClient.SyncUploadFromUri(uri, true, CancellationToken.None);
+ 
+
